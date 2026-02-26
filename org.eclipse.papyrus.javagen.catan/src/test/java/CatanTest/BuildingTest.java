819ae453955913dbac36e5027fe0e1b6cb6092ce
JoooannaF <fanr24@mcmaster.ca>
2026-02-25 22:00:34 -0500

    Test units generated

---
+++ org.eclipse.papyrus.javagen.catan/src/test/java/CatanTest/BuildingTest.java
@@ -0,0 +1,14 @@
+package CatanTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BuildingTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
