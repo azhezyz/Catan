d27e1ff085e2922adf54642d062b8316300a46f2
molly429 <molly060429@gmail.com>
2026-03-01 02:21:20 -0500

    Unit test for DiceSet

--- org.eclipse.papyrus.javagen.catan/src/test/java/CatanTest/DiceSetTest.java
+++ org.eclipse.papyrus.javagen.catan/src/test/java/CatanTest/DiceSetTest.java
@@ -1,4 +1,42 @@
 package CatanTest;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import Catan.DiceSet;
 
 class DiceSetTest {
+
+    @Test
+    void nextRoll_returnsValueBetween2And12() {
+        DiceSet diceSet = new DiceSet();
+
+        for (int i = 0; i < 200; i++) {
+            int result = diceSet.nextRoll();
+            assertTrue(result >= 2 && result <= 12);
+        }
+    }
+    @Test
+    void multipleRolls_allWithinValidRange() {
+        DiceSet diceSet = new DiceSet();
+
+        for (int i = 0; i < 1000; i++) {
+            int result = diceSet.nextRoll();
+            assertTrue(result >= 2 && result <= 12);
+        }
+    }
+
+    @Test
+    void nextRoll_notAlwaysSameValue() {
+        DiceSet diceSet = new DiceSet();
+
+        int first = diceSet.nextRoll();
+        boolean differentFound = false;
+
+        for (int i = 0; i < 100; i++) {
+            if (diceSet.nextRoll() != first) {
+                differentFound = true;
+                break;
+            }
+        }
+        assertTrue(differentFound);
+    }
 }
